15ac30d6cb33c75cad6f385c5c9e43450a956699
Alan Sampson <alansampson@me.com>
2016-10-15 14:01:32 -0700

    Initial project set-up

---
+++ Oh-Hello/src/hello/test/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Oh-Hello:package-info.java:Oct 15, 2016:1:55:45 PM
+ *
+ * (C) Copyright Alan Sampson <alansamps@gmail.com> 2016, All rights reserved.
+ *
+ * #(@) package-info.java $Header: $
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Created: Oct 15, 2016
+ *
+ * -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+ */
+
+/**
+ * @author alansampson : <a href="mailto:%22Alan%20Sampson%22%20%3Calansamps@gmail.com%3E">&quot;Alan Sampson&quot; &lt;alansamps@gmail.com&gt;</a>
+ * @version 0.1
+ *
+ */
+
+package hello.test;
